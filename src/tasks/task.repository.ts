478d13dc768baef46d68451b609446cb34a98d1d
Son-MBP13 <tson0406@gmail.com>
2020-10-06 20:54:47 +0700

    [UPDATE] delete task

---
+++ src/tasks/task.repository.ts
@@ -0,0 +1,20 @@
+import { Task } from './task.entity';
+import { EntityRepository, Repository } from 'typeorm';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskStatus } from './task-status.enum';
+import { NotAcceptableException, NotFoundException } from '@nestjs/common';
+
+@EntityRepository(Task)
+export class TaskRepository extends Repository<Task> {
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    const { title, description } = createTaskDto;
+
+    const task = new Task();
+    task.title = title;
+    task.description = description;
+    task.status = TaskStatus.OPEN;
+    await task.save();
+
+    return task;
+  }
+}
